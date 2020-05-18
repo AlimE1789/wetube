f1eed765072089b01de0212ee5b933d3c9324d9b
Alims <alims1789@cssmart.co.kr>
2020-05-18 11:46:31 +0900

    [2.3] Your First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hi"); 
+const express = require('express') // require is take ('') from Node Module.
+const app = express()
+const port = 4000
+
+function handleListening(){
+    console.log(`Listening on : http://localhost:${port}`)
+}
+
+app.listen(port,handleListening)
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
