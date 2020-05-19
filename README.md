348b400706b4c1bb87eb8b93deda2b541e8001fc
Alims <alims1789@cssmart.co.kr>
2020-05-19 11:44:12 +0900

    2.5 ES6 on NodeJS using Babel

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# WeTube
+# WeTube - 2.5 ES6 on NodeJS using Babel
 Cloning Youtube with Vanilla and NodeJs
+
+1. install @babel/node
+2. install @babel/preset-env
+3. install @babel/core
+4. changed from Node grammar to Babel grammar.
+4. install nodemon
+
+[package.json]
+- dependencies: need lib
+- devDependencies : No need lib
+- nodemon : A tool that aucomatically restart when you save.
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const express = require('express') // require is take ('') from Node Module.
+
+import express from "express" // == const express = require('express') // require is take ('') from Node Module.
+
 const app = express()
 const port = 4000
+ 
+// babel_grammar
+const handleListening = (req,res) => console.log(`Listening on : http://localhost:${port}`)
+const handleHome = (req,res) => res.send("Hello from Home")
+const handleProfile = (req,res) => res.send("You are on my profile") 
 
-function handleListening(){
-    console.log(`Listening on : http://localhost:${port}`)
-}
-
-function handleHome(req,res){
-    res.send("Hello from Home")
-}
-
-function handleProfile(req,res){
-    res.send("You are on my profile")
-}
+//node_grammer
+//
+// function handleListening(){
+//     console.log(`Listening on : http://localhost:${port}`)
+// }
+//
+// function handleHome(req,res){
+//     res.send("Hello from Home")
+// }
+//
+//function handleProfile(req,res){
+// res.send("You are on my profile")
+//}
 
 app.get("/",handleHome)
 app.get("/profile",handleProfile)
